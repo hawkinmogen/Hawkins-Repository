302f654bd5fc294debc8e551a612ab0184418aab
Hawkin Mogen <hawkinmogen@gmail.com>
2020-02-10 01:11:57 -0600

    complete up to coding practice:loops

--- loggingLetters.js
+++ loggingLetters.js
@@ -5,3 +5,24 @@ for (var i=0; i < sentence.length; i++) {
       console.log(sentence[i]);
      }
 }
+
+
+
+/*code practice if statements
+var n=25;
+var result=0;
+
+if(n % 2==0){result=result+10;}
+
+else{result=result-n;}
+*/
+
+
+/*code pratice for loops
+var n=25;
+var sum=0;
+
+for(var q=1; q<n; q++){
+	sum=sum+q;
+	console.log
+}*/
