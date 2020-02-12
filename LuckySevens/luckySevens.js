3687ec970be3ade57d436d413cc3ee946823056e
Hawkin Mogen <hawkinmogen@gmail.com>
2020-02-12 02:44:25 -0600

    results of game being shown, except for startingBet

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -4,6 +4,53 @@
 	Last Modified:
 	*Javascript for luckySevens.html*
 	*/
+	
+
+function rollDice(){
+	return Math.floor(Math.random()*6)+1;
+}
+
+function playGame(){
+	var startingBet= document.getElementById("startingBet").value;
+	var gameMoney = startingBet;
+	var firstDie;
+	var secondDie;
+	var diceSum;
+	var diceRolls=0;
+	var maxWinnings=0;
+	var rollsAtMax=0;
+	
+	while (gameMoney >= 1){
+		firstDie = rollDice();
+		secondDie = rollDice();
+		
+		diceSum= firstDie+secondDie;
+		diceRolls++;
+		
+		if(diceSum != 7){
+			gameMoney--;
+			console.log("lost");
+		}
+		
+		else{
+			gameMoney +=4;
+			if (gameMoney > maxWinnings){
+				maxWinnings=gameMoney;
+				rollsAtMax=diceRolls;
+			}
+			console.log("win");
+		}
+	}
+
+    document.getElementById("startingBet").innerHTML = startingBet;
+	document.getElementById("diceRolls").innerHTML = diceRolls;
+	document.getElementById("maxWinnings").innerHTML = maxWinnings;
+	document.getElementById("rollsAtMax").innerHTML = rollsAtMax;
+	return false;
+     
+}
+
+
 
 function validateItems(){
 	
@@ -30,7 +77,9 @@ function validateItems(){
 	/*If "startingBet" is not empty and meets the regex criteria, 
 		the div with id="gameInfo" will display*/
     document.getElementById("gameInfo").style.display="";
+ 	playGame();
     return false;
 	//False is returned so that the form does not submit and we see the results//
 	
 }
+
