a4aa7143fba0e348e355f258dd35bea3633db7ef
Hawkin Mogen <hawkinmogen@gmail.com>
2020-02-11 19:16:27 -0600

    <hr> and "results" appear if starting bet is valid

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -1,6 +1,16 @@
+/*
+	Author: Hawkin Mogen
+	Created: 2/11/2020
+	Last Modified:
+	*Javascript for luckySevens.html*
+	*/
+
 function validateItems(){
+	
+	/*Regular expression is used to ensure that "startingBet" is a valid dollar amount*/
     var regex = /^\d*(\.{1}\d{1,2})?$/;
-    var startingBet = document.getElementById("startingBet").value;
+    
+	var startingBet = document.getElementById("startingBet").value;
 	
 	
 	if(startingBet=="")
@@ -10,12 +20,17 @@ function validateItems(){
       return false;
     }
 	
-	
+	/*"startingBet" is compared against the regex to ensure it meets the criteria*/
 	if(regex.test(startingBet) == false){
    alert("Input must follow the format: (dollars).(cents)");
    document.getElementById("startingBet").focus();
    return false;
     }
-    
-    return true;
-  }
+	
+	/*If "startingBet" is not empty and meets the regex criteria, 
+		the div with id="gameInfo" will display*/
+    document.getElementById("gameInfo").style.display="";
+    return false;
+	//False is returned so that the form does not submit and we see the results//
+	
+}
