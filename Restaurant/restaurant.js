ddfb4a9ef346b63f402ee7ac5735e5f99fd76f6e
Hawkin Mogen <hawkinmogen@gmail.com>
2020-02-16 02:43:24 -0600

    form validations complete

---
+++ Restaurant/restaurant.js
@@ -0,0 +1,33 @@
+function validateForms(){
+	
+	var nameTextInput = document.getElementById('name').value;
+	var emailTextInput = document.getElementById('email').value;
+	var phoneTextInput = document.getElementById('phone').value;
+    var check = document.getElementsByClassName("form-check-input");
+	
+	if (nameTextInput ==='' || nameTextInput == null){
+		alert("Name field is require.");
+		return false;
+	}
+	
+	if (emailTextInput ==='' || emailTextInput == null){
+		alert("Email field is require.");
+		return false;
+	}
+		
+		if (phoneTextInput ==='' || phoneTextInput == null){
+		alert("Phone field is require.");
+		return false;
+	}
+	
+	if(check[0].checked === false &&
+		check[1].checked === false &&
+		check[2].checked === false &&
+		check[3].checked === false &&
+		check[4].checked === false){
+			alert("Please select atleast one best day to contact you.");
+			return false;
+		}
+	
+	
+}
